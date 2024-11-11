7a027489683d4a507202affca6506f7a44ccd926
gghh200 <tamjones2005@gmail.com>
2024-11-11 16:24:59 +0000

    speaker

---
+++ Project/Speaker.h
@@ -0,0 +1,11 @@
+#include "OneClick.h"
+
+class Speaker : public OneClick{
+        public:
+                inline Speaker(string name, bool& end) : OneClick(name), Device(name){};
+                inline void setVolume(int Volume){ this->Volume = Volume;};
+                inline void volumeUp(){Volume++;};
+                inline void volumeDown(){Volume--;};
+        private:
+                int Volume;
+};
